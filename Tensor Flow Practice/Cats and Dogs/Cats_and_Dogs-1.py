306d63fd9898643a5546be29428457a44a7658f7
dks2000dks <47571301+dks2000dks@users.noreply.github.com>
2019-07-14 16:47:17 +0530

    Add files via upload

---
+++ Tensor Flow Practice/Cats and Dogs/Cats_and_Dogs-1.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+
+# Cat vs. Dog Image Classification
+
+import os
+import tensorflow as tf
+
+# Very Very Important
+"""
+In some cases it is desirable for the process to only allocate a subset of the available memory, or to only grow the memory usage as it is needed by the process.
+TensorFlow provides two configuration options on the session to control this.
+The first is the allow_growth option, which attempts to allocate only as much GPU memory based on runtime allocations, it starts out allocating very little memory,and as sessions get run and more GPU memory is needed, we extend the GPU memory region needed by the TensorFlow process.
+
+# This is done to avoid the following error. "e tensorflow/stream_executor/cuda/cuda_dnn.cc:329] could not create cudnn handle: cudnn_status_internal_error"
+
+"""
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+
+print ("-------------------------------------------------------------------------------------")
+
+
+base_dir = '/home/krishna/Desktop/Image Processing/Tensor Flow Practice/Cats and Dogs/Images'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+#Visualising some of the filenames look like in the `cats` and `dogs` `train` directories
+
+train_cat_fnames = os.listdir(train_cats_dir)
+print (train_cat_fnames[:10])
+
+train_dog_fnames = os.listdir(train_dogs_dir)
+train_dog_fnames.sort()
+print (train_dog_fnames[:10])
+
+"""Let's find out the total number of cat and dog images in the `train` and `validation` directories:"""
+
+print ('total training cat images:', len(os.listdir(train_cats_dir)))
+print ('total training dog images:', len(os.listdir(train_dogs_dir)))
+print ('total validation cat images:', len(os.listdir(validation_cats_dir)))
+print ('total validation dog images:', len(os.listdir(validation_dogs_dir)))
+
+"""For both cats and dogs, we have 1,000 training images and 500 test images.
+
+Now let's take a look at a few pictures to get a better sense of what the cat and dog datasets look like. First, configure the matplot parameters:
+"""
+
+# %matplotlib inline
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# Parameters for our graph; we'll output images in a 4x4 configuration
+nrows = 4
+ncols = 4
+
+# Index for iterating over images
+pic_index = 0
+
+"""Now, display a batch of 8 cat and 8 dog pictures. You can rerun the cell to see a fresh batch each time:"""
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+fig = plt.gcf()
+fig.set_size_inches(ncols * 4, nrows * 4)
+
+pic_index += 8
+next_cat_pix = [os.path.join(train_cats_dir, fname) 
+                for fname in train_cat_fnames[pic_index-8:pic_index]]
+next_dog_pix = [os.path.join(train_dogs_dir, fname) 
+                for fname in train_dog_fnames[pic_index-8:pic_index]]
+
+for i, img_path in enumerate(next_cat_pix+next_dog_pix):
+  # Set up subplot; subplot indices start at 1
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off') # Don't show axes (or gridlines)
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+"""## Building a Small Convnet from Scratch to Get to 72% Accuracy
+
+The images that will go into our convnet are 150x150 color images (in the next section on Data Preprocessing, we'll add handling to resize all the images to 150x150 before feeding them into the neural network).
+
+Let's code up the architecture. We will stack 3 {convolution + relu + maxpooling} modules. Our convolutions operate on 3x3 windows and our maxpooling layers operate on 2x2 windows. Our first convolution extracts 16 filters, the following one extracts 32 filters, and the last one extracts 64 filters.
+
+**NOTE**: This is a configuration that is widely used and known to work well for image classification. Also, since we have relatively few training examples (1,000), using just three convolutional modules keeps the model small, which lowers the risk of overfitting (which we'll explore in more depth in Exercise 2.)
+"""
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+#On top of it we stick two fully-connected layers. Because we are facing a two-class classification problem
+
+# Flatten feature map to a 1-dim tensor so we can add fully connected layers
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(1, activation='sigmoid')(x)
+
+# Create model:
+# input = input feature map
+# output = input feature map + stacked convolution/maxpooling layers + fully 
+# connected layer + sigmoid output layer
+model = Model(img_input, output)
+
+"""Model architecture:"""
+
+print (model.summary())
+print ("--------------------------------------------------------------------")
+
+"""
+The "output shape" column shows how the size of your feature map evolves in each successive layer. The convolution layers reduce the size of the feature maps by a bit due to padding, and each pooling layer halves the feature map.
+"""
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',optimizer=RMSprop(lr=0.001),metrics=['acc'])
+
+
+"""
+Data Preprocessing
+"""
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+"""### Training
+Let's train on all 2,000 images available, for 15 epochs, and validate on all 1,000 test images. (This may take a few minutes to run.)
+"""
+
+history = model.fit_generator(
+		train_generator,
+		steps_per_epoch=100,  # 2000 images = batch_size * steps
+		epochs=15,
+		validation_data=validation_generator,
+		validation_steps=50,  # 1000 images = batch_size * steps
+		verbose=2)
+
+"""### Visualizing Intermediate Representations
+
+To get a feel for what kind of features our convnet has learned, one fun thing to do is to visualize how an input gets transformed as it goes through the convnet.
+
+Let's pick a random cat or dog image from the training set, and then generate a figure where each row is the output of a layer, and each image in the row is a specific filter in that output feature map. Rerun this cell to generate intermediate representations for a variety of training images.
+
+
+import numpy as np
+import random
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+
+# Let's define a new Model that will take an image as input, and will output
+# intermediate representations for all layers in the previous model after
+# the first.
+successive_outputs = [layer.output for layer in model.layers[1:]]
+visualization_model = Model(img_input, successive_outputs)
+
+# Let's prepare a random input image of a cat or dog from the training set.
+cat_img_files = [os.path.join(train_cats_dir, f) for f in train_cat_fnames]
+dog_img_files = [os.path.join(train_dogs_dir, f) for f in train_dog_fnames]
+img_path = random.choice(cat_img_files + dog_img_files)
+
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# Rescale by 1/255
+x /= 255
+
+# Let's run our image through our network, thus obtaining all
+# intermediate representations for this image.
+successive_feature_maps = visualization_model.predict(x)
+
+# These are the names of the layers, so can have them as part of our plot
+layer_names = [layer.name for layer in model.layers]
+
+# Now let's display our representations
+for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+	if len(feature_map.shape) == 4:
+	# Just do this for the conv / maxpool layers, not the fully-connected layers
+		n_features = feature_map.shape[-1]  # number of features in feature map
+	# The feature map has shape (1, size, size, n_features)
+		size = feature_map.shape[1]
+	# We will tile our images in this matrix
+		display_grid = np.zeros((size, size * n_features))
+		for i in range(n_features):
+		# Postprocess the feature to make it visually palatable
+			x = feature_map[0, :, :, i]
+			x -= x.mean()
+			x /= x.std()
+			x *= 64
+			x += 128
+			x = np.clip(x, 0, 255).astype('uint8')
+			# We'll tile each filter into this big horizontal grid
+			display_grid[:, i * size : (i + 1) * size] = x
+		# Display the grid
+		scale = 20. / n_features
+		plt.figure(figsize=(scale * n_features, scale))
+		plt.title(layer_name)
+		plt.grid(False)
+		plt.imshow(display_grid, aspect='auto', cmap='viridis')
+"""
+"""
+Calculating Accuracy and Loss for the Model
+
+Plotting the training/validation accuracy and loss as collected during training:
+"""
+
+# Retrieve a list of accuracy results on training and test data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+plt.show()
---
+++ Tensor Flow Practice/Cats and Dogs/Cats_and_Dogs-2.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+
+# Cat vs. Dog Image Classification
+
+import os
+import tensorflow as tf
+
+# Very Very Important
+"""
+In some cases it is desirable for the process to only allocate a subset of the available memory, or to only grow the memory usage as it is needed by the process.
+TensorFlow provides two configuration options on the session to control this.
+The first is the allow_growth option, which attempts to allocate only as much GPU memory based on runtime allocations, it starts out allocating very little memory,and as sessions get run and more GPU memory is needed, we extend the GPU memory region needed by the TensorFlow process.
+
+# This is done to avoid the following error. "e tensorflow/stream_executor/cuda/cuda_dnn.cc:329] could not create cudnn handle: cudnn_status_internal_error"
+
+"""
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+
+print ("-------------------------------------------------------------------------------------")
+# limitations under the License.
+
+"""# Cat vs. Dog Image Classification
+## Exercise 2: Reducing Overfitting
+**_Estimated completion time: 30 minutes_**
+
+In this notebook we will build on the model we created in Exercise 1 to classify cats vs. dogs, and improve accuracy by employing a couple strategies to reduce overfitting: **data augmentation** and **dropout**. 
+
+We will follow these steps:
+
+1. Explore how data augmentation works by making random transformations to training images.
+2. Add data augmentation to our data preprocessing.
+3. Add dropout to the convnet.
+4. Retrain the model and evaluate loss and accuracy. 
+
+Let's get started!
+
+## Exploring Data Augmentation
+
+Let's get familiar with the concept of **data augmentation**, an essential way to fight overfitting for computer vision models.
+
+In order to make the most of our few training examples, we will "augment" them via a number of random transformations, so that at training time, **our model will never see the exact same picture twice**. This helps prevent overfitting and helps the model generalize better.
+
+This can be done by configuring a number of random transformations to be performed on the images read by our `ImageDataGenerator` instance. Let's get started with an example:
+"""
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+datagen = ImageDataGenerator(
+      rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+"""These are just a few of the options available (for more, see the [Keras documentation](https://keras.io/preprocessing/image/). Let's quickly go over what we just wrote:
+
+- `rotation_range` is a value in degrees (0–180), a range within which to randomly rotate pictures.
+- `width_shift` and `height_shift` are ranges (as a fraction of total width or height) within which to randomly translate pictures vertically or horizontally.
+- `shear_range` is for randomly applying shearing transformations.
+- `zoom_range` is for randomly zooming inside pictures.
+- `horizontal_flip` is for randomly flipping half of the images horizontally. This is relevant when there are no assumptions of horizontal assymmetry (e.g. real-world pictures).
+- `fill_mode` is the strategy used for filling in newly created pixels, which can appear after a rotation or a width/height shift.
+
+Let's take a look at our augmented images. First let's set up our example files, as in Exercise 1.
+
+**NOTE:** The 2,000 images used in this exercise are excerpted from the ["Dogs vs. Cats" dataset](https://www.kaggle.com/c/dogs-vs-cats/data) available on Kaggle, which contains 25,000 images. Here, we use a subset of the full dataset to decrease training time for educational purposes.
+"""
+
+
+import os
+
+base_dir = '/home/krishna/Desktop/Image Processing/Tensor Flow Practice/Cats and Dogs/Images'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+train_cat_fnames = os.listdir(train_cats_dir)
+train_dog_fnames = os.listdir(train_dogs_dir)
+
+"""Next, let's apply the `datagen` transformations to a cat image from the training set to produce five random variants. Rerun the cell a few times to see fresh batches of random variants."""
+
+# %matplotlib inline
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
+
+img_path = os.path.join(train_cats_dir, train_cat_fnames[2])
+img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+
+# The .flow() command below generates batches of randomly transformed images
+# It will loop indefinitely, so we need to `break` the loop at some point!
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+  plt.figure(i)
+  imgplot = plt.imshow(array_to_img(batch[0]))
+  i += 1
+  if i % 5 == 0:
+    break
+
+"""## Add Data Augmentation to the Preprocessing Step
+
+Now let's add our data-augmentation transformations from [**Exploring Data Augmentation**](#scrollTo=E3sSwzshfSpE) to our data preprocessing configuration:
+"""
+
+# Adding rescale, rotation_range, width_shift_range, height_shift_range,
+# shear_range, zoom_range, and horizontal flip to our ImageDataGenerator
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 32 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        train_dir,  # This is the source directory for training images
+        target_size=(150, 150),  # All images will be resized to 150x150
+        batch_size=20,
+        # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
+
+# Flow validation images in batches of 32 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=20,
+        class_mode='binary')
+
+"""If we train a new network using this data augmentation configuration, our network will never see the same input twice. However the inputs that it sees are still heavily intercorrelated, so this might not be quite enough to completely get rid of overfitting.
+
+## Adding Dropout
+
+Another popular strategy for fighting overfitting is to use **dropout**.
+
+**TIP:** To learn more about dropout, see [Training Neural Networks](https://developers.google.com/machine-learning/crash-course/training-neural-networks/video-lecture) in [Machine Learning Crash Course](https://developers.google.com/machine-learning/crash-course/).
+
+Let's reconfigure our convnet architecture from Exercise 1 to add some dropout, right before the final classification layer:
+"""
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.optimizers import RMSprop
+
+# Our input feature map is 150x150x3: 150x150 for the image pixels, and 3 for
+# the three color channels: R, G, and B
+img_input = layers.Input(shape=(150, 150, 3))
+
+# First convolution extracts 16 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(16, 3, activation='relu')(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Second convolution extracts 32 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Conv2D(32, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Third convolution extracts 64 filters that are 3x3
+# Convolution is followed by max-pooling layer with a 2x2 window
+x = layers.Convolution2D(64, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Flatten feature map to a 1-dim tensor
+x = layers.Flatten()(x)
+
+# Create a fully connected layer with ReLU activation and 512 hidden units
+x = layers.Dense(512, activation='relu')(x)
+
+# Add a dropout rate of 0.5
+x = layers.Dropout(0.5)(x)
+x = layers.Dense(2, activation='softmax')(x)
+
+# Create output layer with a single node and sigmoid activation
+output = layers.Dense(2, activation='sigmoid')(x)
+
+# Configure and compile the model
+model = Model(img_input, output)
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=RMSprop(lr=0.0001),
+              metrics=['acc'])
+
+"""## Retrain the Model
+
+With data augmentation and dropout in place, let's retrain our convnet model. This time, let's train on all 2,000 images available, for 30 epochs, and validate on all 1,000 test images. (This may take a few minutes to run.) See if you can write the code yourself:
+"""
+
+model.summary()
+
+"""### Solution
+
+Click below for the solution.
+"""
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=100,
+      epochs=10,
+      validation_data=validation_generator,
+      validation_steps=100,
+      verbose=2)
+
+"""Note that with data augmentation in place, the 2,000 training images are randomly transformed each time a new training epoch runs, which means that the model will never see the same image twice during training.
+
+## Evaluate the Results
+
+Let's evaluate the results of model training with data augmentation and dropout:
+"""
+
+# Retrieve a list of accuracy results on training and test data
+# sets for each training epoch
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+# Get number of epochs
+epochs = range(len(acc))
+
+# Plot training and validation accuracy per epoch
+plt.plot(epochs, acc)
+plt.plot(epochs, val_acc)
+plt.title('Training and validation accuracy')
+
+plt.figure()
+
+# Plot training and validation loss per epoch
+plt.plot(epochs, loss)
+plt.plot(epochs, val_loss)
+plt.title('Training and validation loss')
+plt.show()
+
+print ("---------------------------------------------------------------")
+# Predictions
+import numpy as np
+import PIL.Image as Image
+
+Input_Image_Shape = (150, 150)
+
+img = "Cat.jpg"
+img = Image.open(img).resize(Input_Image_Shape)
+plt.imshow(img)
+
+
+img = np.array(img)/255.0
+print (img.shape)
+
+Possible_Outputs = ["Cat","Dog"]
+print (Possible_Outputs)
+
+Prediction = model.predict(img[np.newaxis, ...])
+print (Prediction.shape)
+
+Predicted_class = np.argmax(Prediction[0], axis=-1)
+print (Predicted_class)
+
+print (Possible_Outputs[Predicted_class])
